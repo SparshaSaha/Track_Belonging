355ac6e20ad3d180e97928cc16b518d887190c36
Sparsha Saha <saha.sparsha@gmail.com>
2017-09-08 16:07:14 +0530

    changes

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 
-var db = mongoose.connection.openUri('mongodb://127.0.0.1:27017/TrackerDatabase3');
-
+var db = mongoose.connection.openUri('mongodb://heroku_d2cs28g3:hmsbs2a4sf8252piktq4438o1e@ds127894.mlab.com:27894/heroku_d2cs28g3');
+//mongodb://heroku_d2cs28g3:hmsbs2a4sf8252piktq4438o1e@ds127894.mlab.com:27894/heroku_d2cs28g3
+//'mongodb://127.0.0.1:27017/TrackerDatabase3'
 mongoose.connection.once('connected',function(){
   console.log("Connected to database");
 });
